85fa96069e805746056845a4a4b684a114389edd
volant360 <efrainh12@galileo.edu>
2019-09-16 11:33:16 -0600

    Template Home and Default User Management
    I created a temporal homepage for devise to redirect to and I also created views and controllers for the user management

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :registerable, :trackable and :omniauthable
+  devise :database_authenticatable, :lockable, :timeoutable,
+         :recoverable, :rememberable, :validatable, :registerable
+end
