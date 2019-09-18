23b061bf44610f4926c186007dc50cc314ec4a9e
volant360 <efrainh12@galileo.edu>
2019-09-18 09:51:14 -0600

    Views for Measure Units
    I created a scaffold for the measure units and added the same styles that the ones on the clients views

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,4 +1,4 @@
-class ClientsController < ApplicationController
+class ClientsController < ApplicationLoggedController
   layout "manager"
   before_action :set_client, only: [:show, :edit, :update, :destroy]
   
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_18_130516) do
+ActiveRecord::Schema.define(version: 2019_09_18_143604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2019_09_18_130516) do
     t.text "address"
     t.string "nit"
     t.string "phone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "measure_units", force: :cascade do |t|
+    t.string "name"
+    t.string "unit_type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
