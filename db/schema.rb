7f6f7c70950b65cddc665154a12a60a4945435df
volant360 <efrainh12@galileo.edu>
2019-09-18 07:49:40 -0600

    scaffold and index view for clients
    I created a scaffold for the clients table and changed the index view so it matched our design

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_16_170918) do
+ActiveRecord::Schema.define(version: 2019_09_18_130516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.string "name"
+    t.text "address"
+    t.string "nit"
+    t.string "phone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
