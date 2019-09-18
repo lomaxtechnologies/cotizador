4ee27fecc715864580dc00d42f9e5454fcaad19d
volant360 <efrainh12@galileo.edu>
2019-09-18 11:59:48 -0600

    Views for Users
    I created a new controller to manages users as in CRUD and changed the styles to match the application ones

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,62 +1,26 @@
-# frozen_string_literal: true
+class Users::RegistrationsController < ApplicationLoggedController
+  layout "manager"
+  before_action :set_registration, only: [:show, :edit, :update, :destroy]
 
-class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
-
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
-
-  # POST /resource
-  def create
-    super
-   end
-
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource
-  # def update
-  #   super
-  # end
-
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:user_type])
+  # GET /clients
+  # GET /clients.json
+  def index
+    @users = User.all
   end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  # GET /clients/new
+  def new
+    @user = User.new
+  end
 
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  # GET /clients/1/edit
+  def edit
+  end
 
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_registration
+      @user = User.find(params[:id])
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
+  
+  namespace :users do
+    resources :registrations
+  end
   resources :measure_units
   resources :clients
+  
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # Overriding routes to make them point to generated controllers
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     passwords: 'users/passwords',
-    unlocks: 'users/unlocks'
+    unlocks: 'users/unlocks',
+    registrations: 'users/registrations'
   }
 
 end
