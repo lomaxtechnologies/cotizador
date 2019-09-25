8cc91cc8cf2149d17e46407b0c599e504861f26d
Luis Fernando Dávila de León <54920824+luisferyimi19@users.noreply.github.com>
2019-09-25 07:58:26 -0600

    Change holgura
    holgura change to percent from product

--- db/migrate/20190925033030_create_quotation_product_and_references.rb
+++ db/migrate/20190925033030_create_quotation_product_and_references.rb
@@ -2,7 +2,7 @@ class CreateQuotationProductAndReferences < ActiveRecord::Migration[6.0]
   def change
     create_table :quotation_products do |t|
       t.decimal :amount
-      t.decimal :holgura
+      t.decimal :percent
       t.references :quotation, null: false, foreign_key: true
       t.references :product, null: false, foreign_key: true
       t.datetime :deleted_at, index: true
