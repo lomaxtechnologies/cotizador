ab35a3e373b8eb0ec1bd239ff1311914be03ab76
Ilusión Santos <ilusion.santos@galileo.edu>
2019-10-01 11:05:00 -0600

    Added rules for parser excel

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,10 @@
 class Product < ApplicationRecord
   belongs_to :material
-  belongs_to :brand
+  belongs_to :brand, optional: true
   belongs_to :measure_unit
   has_one :price
   has_many :quotation_products
   has_many :comments, as: :commentable
+
+
 end
--- app/services/materials_parser.rb
+++ app/services/materials_parser.rb
@@ -20,17 +20,30 @@ class MaterialsParser
       end
       if(code?(row.cells[0].value))
         code = row.cells[0].value
-        #measure_unit_id = MeasureUnit.find(name: 'Unidad')
+        measure_unit_id = MeasureUnit.find_by(name: 'Unidad')
         price = row.cells[2].value.to_s.gsub(/[^\d^\.]/, '').to_f
         data = row.cells[1].value.split(",")
+        brand = (data[0].split(" "))[0]
+        brand_id = nil
+        if(!!(brand=~/Siemon/)||!!(brand=~/Supranet/))
+          brand_id = Brand.find_by(name: brand)
+        end
+        name = data[0]
+        description = row.cells[1].value
         #brand = 
-        #material_id = find_material(code, name, description)
+        material_id = find_material(code, name, description)
         #print price
         if (@imacable)
-          measure_unit_ida = MeasureUnit.find(name: 'Bobina')
-          measure_unit_idb = MeasureUnit.find(name: 'Metro')
+          measure_unit_ida = MeasureUnit.find_by(name: 'Bobina')
+          measure_unit_idb = MeasureUnit.find_by(name: 'Metro')
           pricem = price/305 
+          product_ida = find_product(brand_id,material_id,measure_unit_ida)
+          Price.create_with(prices: price).find_or_create_by(product_ida)
+          product_idb = find_product(brand_id,material_id,measure_unit_idb)
+          Price.create_with(prices: pricem).find_or_create_by(product_idb)
         end
+        product_id = find_product(brand_id,material_id,measure_unit_id)
+        #Price.create_with(prices: price).find_or_create_by(product_id)
       end
     end
   end
@@ -38,7 +51,9 @@ class MaterialsParser
   def code?(str)
     !!(str =~ /^LMX-\d{5,8}/)
   end
-
+  def find_product(brand_id,material_id,measure_id)
+    return id = Product.find_or_create_by(material_id: material_id,measure_unit_id: measure_id,brand_id: brand_id)
+  end
   def find_material(code,name,description)
     return id = Material.create_with(name: name, description: description).find_or_create_by(code: code) 
   end
