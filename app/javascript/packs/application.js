9fd809b957de1dd0aa05c06a252024ba59b2afec
Luisferyimi19 <ldavilad@miumg.edu.gt>
2019-10-03 08:19:33 -0600

    #12 Template Theme bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,8 @@ require("channels")
 
 import '../stylesheets/application'
 import './bootstrap_custom.js'
+import '../stylesheets/vendors/@coreui/coreui-pro/js/coreui.min.js'
+import '../stylesheets/vendors/@coreui/coreui-pro/js/coreui.min.js.map'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
