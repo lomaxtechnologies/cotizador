0f6e799cfac0d4278a28232a4660ea1a8e17b5ed
Luisferyimi19 <ldavilad@miumg.edu.gt>
2019-10-03 12:01:42 -0600

    Navbar styles

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,7 @@ import '../stylesheets/application'
 import './bootstrap_custom.js'
 import '../stylesheets/vendors/@coreui/coreui-pro/js/coreui.min.js'
 import '../stylesheets/vendors/@coreui/coreui-pro/js/coreui.min.js.map'
+import '../stylesheets/vendors/perfect-scrollbar/js/perfect-scrollbar.min.js'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
