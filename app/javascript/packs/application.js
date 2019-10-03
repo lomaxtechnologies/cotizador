1f14f7329df8b579fed013c270f4bb4bbf43eb61
volant360 <efrainh12@galileo.edu>
2019-10-03 11:03:34 -0600

    Added new Style to the Login
    using coreIU Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
 import '../stylesheets/application'
 import './bootstrap_custom.js'
-import '../stylesheets/vendors/@coreui/coreui-pro/js/coreui.min.js'
-import '../stylesheets/vendors/@coreui/coreui-pro/js/coreui.min.js.map'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
