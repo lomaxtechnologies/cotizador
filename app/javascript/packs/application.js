95a1d10bb6eb2b0c7af57521b3225f731c051644
volant360 <efrainh12@galileo.edu>
2019-10-03 12:04:58 -0600

    Notifications
    Added style to the notifications

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 import '../stylesheets/application'
 import './bootstrap_custom.js'
-
+import '../application/notification_handler.js'
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
