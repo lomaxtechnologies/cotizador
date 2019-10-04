fe086161c113382bb242d166258328036566ce83
volant360 <efrainh12@galileo.edu>
2019-10-04 08:02:15 -0600

    Changes to Materials Style
    I changed some texts and styles to fit the rest of the application

--- app/javascript/brands/index.js
+++ app/javascript/brands/index.js
@@ -1,17 +1,17 @@
-const CONFIRM_DELETE_USER_TITLE = '¿Realmente deseas eliminiar a esta marca?';
-const CONFIRM_DELETE_USER_BODY = `
+const CONFIRM_DELETE_BRAND_TITLE = '¿Realmente deseas eliminiar a esta marca?';
+const CONFIRM_DELETE_BRAND_BODY = `
    Una vez eliminada no podrás recuperarla y deberás crear una nueva. 
    Todas la información asociada a esta marca (productos, cotizaciones, etc.) seguirá existiendo.
 `;
-const CONFIRM_DELETE_USER_YES_TEXT = 'Sí, elimínala';
-const CONFIRM_DELETE_USER_NO_TEXT = 'Cancelar';
+const CONFIRM_DELETE_BRAND_YES_TEXT = 'Sí, elimínala';
+const CONFIRM_DELETE_BRAND_NO_TEXT = 'Cancelar';
 
 window.show_confirmation_modal = (element)=>{
    element = $(element);
-   confirmation_modal.set_title(CONFIRM_DELETE_USER_TITLE);
-   confirmation_modal.set_body(CONFIRM_DELETE_USER_BODY);
-   confirmation_modal.set_yes_text(CONFIRM_DELETE_USER_YES_TEXT);
-   confirmation_modal.set_no_text(CONFIRM_DELETE_USER_NO_TEXT);
+   confirmation_modal.set_title(CONFIRM_DELETE_BRAND_TITLE);
+   confirmation_modal.set_body(CONFIRM_DELETE_BRAND_BODY);
+   confirmation_modal.set_yes_text(CONFIRM_DELETE_BRAND_YES_TEXT);
+   confirmation_modal.set_no_text(CONFIRM_DELETE_BRAND_NO_TEXT);
    var yes_id = confirmation_modal.yes_button_id;
    confirmation_modal.set_attr(yes_id,'href',element.attr('action'));
    confirmation_modal.set_attr(yes_id,'data-method','delete');
