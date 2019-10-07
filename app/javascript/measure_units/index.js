e7a63a0ddaa66d0e847254e547d240d685b3a224
volant360 <efrainh12@galileo.edu>
2019-10-07 11:31:49 -0600

    i18n-js config for materials, brands and measure units

--- app/javascript/measure_units/index.js
+++ app/javascript/measure_units/index.js
@@ -1,17 +1,9 @@
-const CONFIRM_DELETE_MEASURE_UNIT_TITLE = '¿Realmente deseas eliminiar a esta unidad de medida?';
-const CONFIRM_DELETE_MEASURE_UNIT_BODY = `
-   Una vez eliminada no podrás recuperarla y deberás crear una nueva. 
-   Todas la información asociada a esta unidad de medida (productos, precios, etc.) seguirá existiendo.
-`;
-const CONFIRM_DELETE_MEASURE_UNIT_YES_TEXT = 'Sí, elimínala';
-const CONFIRM_DELETE_MEASURE_UNIT_NO_TEXT = 'Cancelar';
-
 window.show_confirmation_modal = (element)=>{
    element = $(element);
-   confirmation_modal.set_title(CONFIRM_DELETE_MEASURE_UNIT_TITLE);
-   confirmation_modal.set_body(CONFIRM_DELETE_MEASURE_UNIT_BODY);
-   confirmation_modal.set_yes_text(CONFIRM_DELETE_MEASURE_UNIT_YES_TEXT);
-   confirmation_modal.set_no_text(CONFIRM_DELETE_MEASURE_UNIT_NO_TEXT);
+   confirmation_modal.set_title(I18n.t('measure_units.index.delete_modal.title'));
+   confirmation_modal.set_body(I18n.t('measure_units.index.delete_modal.body'));
+   confirmation_modal.set_yes_text(I18n.t('measure_units.index.delete_modal.yes_text'));
+   confirmation_modal.set_no_text(I18n.t('measure_units.index.delete_modal.no_text'));
    var yes_id = confirmation_modal.yes_button_id;
    confirmation_modal.set_attr(yes_id,'href',element.attr('action'));
    confirmation_modal.set_attr(yes_id,'data-method','delete');
--- app/javascript/packs/materials/index.js
+++ app/javascript/packs/materials/index.js
@@ -1,4 +1,5 @@
 import 'bootstrap/js/dist/modal'
 import 'bootstrap/js/dist/tooltip'
-import '../../application/tooltip_initializer.js'
-import '../../materials/index.js'
+import '../../application/tooltip_initializer'
+import '../../application/locale_config'
+import '../../materials/index'
