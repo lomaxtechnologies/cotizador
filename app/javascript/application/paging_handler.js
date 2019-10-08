177f7646332063c70964b60568c098d2d8ed24d0
volant360 <efrainh12@galileo.edu>
2019-10-08 12:02:38 -0600

    Redirected to current URL in paging_handler
    I removed the attribute that indicated the url and redirected to the current url

--- app/javascript/application/paging_handler.js
+++ app/javascript/application/paging_handler.js
@@ -1,13 +1,16 @@
 /*
-   Sets the page size of the window. element is a HTML element that has the url to be redirected to
+   Sets the page size of the window. 'element' is an input that has the new number of pages
    (Usually it is the index page). It ignores the 'page' param and overwrites the 'page_size' param
+   in the url
 */
 window.set_page_size = (element)=>{
    element = $(element);
    if(element[0].validity.valid && element.val()){
       var page_size = parseInt(element.val()) || '';
       var new_query_params = '';
-      var params = window.location.href.split('?')[1] || '';
+      var url_split = window.location.href.split('?');
+      var params = url_split[1] || '';
+      var url = url_split[0];
       params = params.split('&');
       for(var i = 0; i < params.length; i++){
          var param = params[i];
@@ -18,9 +21,6 @@ window.set_page_size = (element)=>{
          new_query_params+=`${param}&`;
       }
       new_query_params+=`page_size=${page_size}`;
-      var url = element.attr('url');
-      if(url){
-         window.location.href = `${url}?${new_query_params}`;
-      }
+      window.location.href = `${url}?${new_query_params}`;
    }
 }
