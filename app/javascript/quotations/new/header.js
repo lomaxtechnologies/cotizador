c2b54471f32cafc20b064ef9313df47e1137c88f
volant360 <efrainh12@galileo.edu>
2019-10-09 10:40:03 -0600

    Quick Commit to Merge Master into this branch

--- app/javascript/quotations/new/header.js
+++ app/javascript/quotations/new/header.js
@@ -6,7 +6,13 @@ Vue.use(TurbolinksAdapter)
 var app = new Vue({
    el: '#quotation_header',
    data:{
-      quotation_type: 1
+      quotation_type: 0,
+      number: 0,
+      client: 0,
+      date: '',
+      percentage_supranet: 0,
+      percentage_siemon: 0,
+      percentage_service: 0
    }
    ,methods:{
    },
