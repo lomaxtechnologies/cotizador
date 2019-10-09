4ba49db3b35fa2bf68ccfb2886ccfd2aa905cf23
volant360 <efrainh12@galileo.edu>
2019-10-09 12:52:17 -0600

    Quotation type
    now the user can select the quotation type and fields will appear and dissapear depending on that

--- app/javascript/quotations/new/header.js
+++ app/javascript/quotations/new/header.js
@@ -1,21 +1,27 @@
 import TurbolinksAdapter from 'vue-turbolinks'
 import Vue from 'vue/dist/vue.esm'
+require('jquery')
 
 Vue.use(TurbolinksAdapter)
 
 var app = new Vue({
    el: '#quotation_header',
    data:{
-      quotation_type: 0,
-      number: 0,
-      client: 0,
-      date: '',
-      percentage_supranet: 0,
-      percentage_siemon: 0,
-      percentage_service: 0
+      quotation_type: 1,
+      valid_header: false,
+      supranet_enabled: true,
+      siemon_enabled: true
    }
    ,methods:{
-   },
-   mounted:function(){
+      validate_header: function(){
+         $('#nav-link-header').removeClass('text-muted').addClass('text-success');
+         this.valid_header = true;
+      },
+      invalidate_header: function(){
+         if(this.valid_header){
+            $('#nav-link-header').removeClass('text-success').addClass('text-muted');
+            this.valid_header = false;
+         }
+      }
    }
 });
