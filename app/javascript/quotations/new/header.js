00f6d43dbdff40f9f9d7a048ffbc6eedc3ed6046
volant360 <efrainh12@galileo.edu>
2019-10-09 10:21:15 -0600

    Working Vue
    Vue now works on header_partial on quotations, and links the quotation type select with the percentage inputs

--- app/javascript/quotations/new/header.js
+++ app/javascript/quotations/new/header.js
@@ -1,1 +1,15 @@
-import Vue from 'vue'
+import TurbolinksAdapter from 'vue-turbolinks'
+import Vue from 'vue/dist/vue.esm'
+
+Vue.use(TurbolinksAdapter)
+
+var app = new Vue({
+   el: '#quotation_header',
+   data:{
+      quotation_type: 1
+   }
+   ,methods:{
+   },
+   mounted:function(){
+   }
+});
