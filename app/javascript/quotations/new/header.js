e7dd737a0c4e9e7a50a5e370ad7c976b1c837143
volant360 <efrainh12@galileo.edu>
2019-10-10 10:03:33 -0600

    Branch on Hold

--- app/javascript/quotations/new/header.js
+++ app/javascript/quotations/new/header.js
@@ -11,12 +11,28 @@ var app = new Vue({
       valid_header: false,
       supranet_enabled: true,
       siemon_enabled: true
-   }
-   ,methods:{
+   },
+   
+   methods:{
+      parse_data: function(data){
+         data = data.split('&');
+         var new_data = {};
+         for(var i = 0; i < data.length; i++){
+            var pair = data[i].split('=');
+            var key = pair[0];
+            new_data[decodeURIComponent(pair[0])] = pair[1];
+         }
+         return new_data;
+      },
+
       validate_header: function(){
          $('#nav-link-header').removeClass('text-muted').addClass('text-success');
+         var data = $("#quotation_header_form").serialize();
+         data = this.parse_data(data);
+         console.log(JSON.stringify(data));
          this.valid_header = true;
       },
+
       invalidate_header: function(){
          if(this.valid_header){
             $('#nav-link-header').removeClass('text-success').addClass('text-muted');
