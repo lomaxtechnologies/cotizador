1d65589738d4d36a3700fa8786bb39b092ee3755
volant360 <efrainh12@galileo.edu>
2019-10-13 13:27:18 -0600

    Added js files and fixed clients module
    I added the js files for all the modules and changed the cliends module views so they would use "javascript_include_tag" instead of "javascript_pack_tag"

---
+++ app/assets/javascript/materials/index.js
@@ -0,0 +1,12 @@
+window.show_confirmation_modal = (element)=>{
+   element = $(element);
+   confirmation_modal.set_title(I18n.t('materials.index.delete_modal.title'));
+   confirmation_modal.set_body(I18n.t('materials.index.delete_modal.body'));
+   confirmation_modal.set_yes_text(I18n.t('materials.index.delete_modal.yes_text'));
+   confirmation_modal.set_no_text(I18n.t('materials.index.delete_modal.no_text'));
+   var yes_id = confirmation_modal.yes_button_id;
+   confirmation_modal.set_attr(yes_id,'href',element.attr('action'));
+   confirmation_modal.set_attr(yes_id,'data-method','delete');
+   confirmation_modal.add_class(yes_id,'btn-danger');
+   $(`#${confirmation_modal.name}`).modal();
+}
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,13 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.precompile += %w( i18n.js )
+
+# Utils files for modals, inline forms and paging
+Rails.application.config.assets.precompile += %w( utils/confirmation_modal.js )
+Rails.application.config.assets.precompile += %w( utils/inline_form_handler.js )
+Rails.application.config.assets.precompile += %w( utils/paging_handler.js )
+
+Rails.application.config.assets.precompile += %w( clients/index.js )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
