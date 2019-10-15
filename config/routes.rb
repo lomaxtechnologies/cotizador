09f819134e44170854e272546911e462fb3c2a07
volant360 <efrainh12@galileo.edu>
2019-10-15 09:45:16 -0600

    Hello World in Vue and dependencies
    Integration with vue router is now complete

--- config/routes.rb
+++ config/routes.rb
@@ -42,7 +42,6 @@ Rails.application.routes.draw do
 
   resources :brands, except: [:edit, :show, :new]
 
-  resource :quotations
-
+  get 'quotations', to: 'quotations#index'
   root to: 'quotations#index'
 end
--- viewsrc/modules/quotations/quotations.js
+++ viewsrc/modules/quotations/quotations.js
@@ -1,1 +1,33 @@
-console.log('Hola mundos');
+// Loading framework and libraries
+import Vue from "vue";
+import VueRouter from "vue-router";
+import BootstrapVue from "bootstrap-vue";
+
+// Loading app vue components
+import quotationsNew from './new.vue';
+import quotationsList from './list.vue';
+
+Vue.use(VueRouter);
+Vue.use(BootstrapVue);
+
+var appRouter = new VueRouter({
+   routes: [
+      {
+         path: '/', redirect: '/list'
+      },
+      {
+         path: '/list', name: 'list', component: quotationsList
+      },
+      {
+         path: '/new', name: 'new', component: quotationsNew
+      }
+   ]
+ });
+
+ 
+
+// Vue app
+let app = new Vue({router: appRouter});
+app.$mount("#quotations_app");
+
+console.log('app mounted');
