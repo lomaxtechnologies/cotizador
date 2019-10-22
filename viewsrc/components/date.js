e37297e7cf351779a4badd7e26a950c7bf53b61f
volant360 <efrainh12@galileo.edu>
2019-10-22 12:02:40 -0600

    Filters working at quotations#index
    I also removed the javascript date conversion and made it manually because it was substracting a day

--- viewsrc/components/date.js
+++ viewsrc/components/date.js
@@ -1,15 +1,22 @@
 export default {
    install (Vue, options) {
-      var date_options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+
       //date will be returned in standard format YYYY-MM-DD
       let today = function(){
          var date = new Date();
          return date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate();
       }
+
+      //date should be given in standard format YYYY-MM-DD
       let toLocalFormat = function(date){
-         date = new Date(date);
-         return date.toLocaleDateString(I18n.currentLocale(),date_options);
+         var parsed_date = I18n.t('formats.local_date');
+         date = date.split('-');
+         return parsed_date
+         .replace('%Y',date[0])
+         .replace('%m',date[1])
+         .replace('%d',date[2]);
       }
+      
       Vue.prototype.date = {
          today,
          toLocalFormat
