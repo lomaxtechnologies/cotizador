b0d1d1d58fa4ff0be045628cd7736c9a85577c8a
Ilusión Santos <ilusion.santos@galileo.edu>
2019-10-25 12:24:52 -0600

    Fixed some problems after merge

--- viewsrc/modules/quotations/show.vue
+++ viewsrc/modules/quotations/show.vue
@@ -6,45 +6,33 @@
   import componentCommentList from '../comments/list.vue'
 
    export default {
-     data(){
-       return {
-          translation:{
-            show: I18n.t('quotations.show')
-          }
-       }
-     },
-     methods:{
-       generateExcel: function(){
-         console.log(this.$route.params.id);
-         let quotation_id = this.$route.params.id;
-         this.http
-         .post(`/quotations/${quotation_id}/excel`)
-         .catch(err => {
-          console.log(JSON.stringify(err));
-          });
-       }
-     },
-     components:{
-      'global-view': globalView,
-      'component-comment-form': componentCommentForm,
-      'component-comment-list': componentCommentList,
-      "quotation-attachments": quotationAttachments
-    },
     data() {
       return {
         translations: I18n.t("quotations.show")
       }
+    },
+    methods:{
+      generateExcel: function(){
+        console.log(this.$route.params.id);
+        let quotation_id = this.$route.params.id;
+        this.http
+        .post(`/quotations/${quotation_id}/excel`)
+        .catch(err => {
+        console.log(JSON.stringify(err));
+        });
+      }
+    },
+    components:{
+      'global-view': globalView,
+      'component-comment-form': componentCommentForm,
+      'component-comment-list': componentCommentList,
+      "quotation-attachments": quotationAttachments
     }
   }
 </script>
 
 <template>
   <div class="row" >
-    <div class="col-lg-12 offset-xl-1 col-xl-10 text-right">
-      <b-button variant="primary" v-on:click="generateExcel">
-        {{this.translation.show.generate_Excel}}
-      </b-button>
-    </div>
     <div class="col-lg-12 offset-xl-1 col-xl-10">
       <div class="row">
         <div class="col-8">
@@ -63,6 +51,10 @@
           <b-button variant="info" class="text-white">
             <i class="fas fa-clone"></i>
             {{translations.buttons.clone}}
+          </b-button>
+          <b-button variant="primary" class="text-white" v-on:click="generateExcel">
+            <i class="fas fa-file"></i>
+            {{this.translations.generate_Excel}}
           </b-button>
         </div>
         <div class="col-4 text-right">
