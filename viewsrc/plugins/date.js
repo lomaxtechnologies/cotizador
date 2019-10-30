2f2a3f12771bcdc852be7b8b183b79f29110e7ce
volant360 <efrainh12@galileo.edu>
2019-10-29 20:24:18 -0600

    Format tables in dashboard and date format
    I also added catch clauses

--- viewsrc/plugins/date.js
+++ viewsrc/plugins/date.js
@@ -7,17 +7,29 @@ export default {
          return date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate();
       }
 
-      //date should be given in standard format YYYY-MM-DD
-      let toLocalFormat = function(date){
-         if(! date){
-            return '';
+      let date_options = {
+         year: 'numeric',
+         month: 'numeric',
+         day: 'numeric',
+         timeZone: 'UTC'
+      };
+
+      let datetime_options = {
+         ...date_options, ...{
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric'
          }
-         var parsed_date = I18n.t('formats.local_date');
-         date = date.split('-');
-         return parsed_date
-         .replace('%Y',date[0])
-         .replace('%m',date[1])
-         .replace('%d',date[2]);
+      }
+
+      //date should be given in standard format YYYY-MM-DD
+      let toLocalFormat = function(date,include_time = false){
+         date = new Date(date);
+         if(include_time){
+            return date.toLocaleDateString(I18n.currentLocale(), datetime_options);
+         }else{
+            return date.toLocaleDateString(I18n.currentLocale(), date_options);
+         }
       }
       
       Vue.prototype.date = {
